1cd176d236c801357b129b2b1f24783aef9d596b
gefangenimnetz <kontakt@florian-kutschera.de>
2016-01-10 00:11:56 +0100

    Initial commit

---
+++ platforms/ios/www/js/index.js
@@ -0,0 +1,80 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+
+        // Init widgets
+        initDatetime();
+        initVVS();
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    },
+    tick: function(){
+        datetimeUpdate();
+    }
+};
+
+app.initialize();
+setInterval(app.tick, 1000);
+
+var dateTimeWidgetNode = document.getElementById('widget_datetime');
+function datetimeUpdate() {
+    dateTimeWidgetNode.innerHTML = moment().format("dddd, D.M.YYYY, H:mm:ss");
+}
+function initDatetime(){
+    moment.locale('de');
+}
+
+var vvsNode = document.getElementById('widget_vvs');
+function initVVS(){
+    var vvsJsonURL = 'https://efa-api.asw.io/api/v1/station/5006133/departures/?format=json';
+    fetch(vvsJsonURL).then(function(response) { 
+        // Convert to JSON
+        return response.json();
+    }).then(function(j) {
+        // Yay, `j` is a JavaScript object
+        vvsNode.innerHTML = JSON.stringify(j);
+        console.log(j); 
+    });
+}
