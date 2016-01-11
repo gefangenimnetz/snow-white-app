89944ff1a1c6dba2a045069a57154ae487840b12
gefangenimnetz <kontakt@florian-kutschera.de>
2016-01-11 21:35:48 +0100

    Preparations for forecast weather

--- www/js/weather/weather.js
+++ www/js/weather/weather.js
@@ -1,12 +1,13 @@
 var weather = {
     node: document.getElementById('widget_weather'),
-    apiUrl: 'http://api.openweathermap.org/data/2.5/weather?q=' + CONFIG.weather.city + '&units=' + CONFIG.weather.units + '&appid=' + CONFIG.weather.appid,
+    apiUrlNow: 'http://api.openweathermap.org/data/2.5/weather?q=' + CONFIG.weather.city + '&units=' + CONFIG.weather.units + '&appid=' + CONFIG.weather.appid,
+    //apiUrlForecast: 'http://api.openweathermap.org/data/2.5/forecast/daily?q=' + CONFIG.weather.city + '&units=' + CONFIG.weather.units + '&appid=' + CONFIG.weather.appid,
 
     init: function(){
-        this.fetchWeatherJSON(this.apiUrl);
+        this.fetchWeatherJSON(this.apiUrlNow);
 
         this.interval = setInterval(function() {
-            this.fetchWeatherJSON();
+            this.fetchWeatherJSON(this.apiUrlNow);
         }.bind(this), 1000 * 60 * 60); //1 hour
     },
 
@@ -21,11 +22,14 @@ var weather = {
     update: function(weather){
         this.node.innerHTML = ''+ 
             '<span class="weather__today">' +
-                '<span class="weather__icon"><i class="wi wi-owm-' + weather.weather[0].id + '"></i></span>' +
+                '<i class="weather__icon wi wi-owm-' + weather.weather[0].id + '"></i>' +
                 '<span class="weather__temp">' + Math.round(weather.main.temp) + '°</span>' +
             '</span>' +
-            '<ui class="weather__forecast">' +
-            '</ui>' +
+            '<ul class="weather__forecast">' +
+                '<li><span class="weather__day">Di</span><i class="weather__icon wi wi-owm-600"></i><span class="weather__temp">9°</span></li>' +
+                '<li><span class="weather__day">Mi</span><i class="weather__icon wi wi-owm-600"></i><span class="weather__temp">9°</span></li>' +
+                '<li><span class="weather__day">Do</span><i class="weather__icon wi wi-owm-600"></i><span class="weather__temp">9°</span></li>' +
+            '</ul>' +
         '';
     }
 }
