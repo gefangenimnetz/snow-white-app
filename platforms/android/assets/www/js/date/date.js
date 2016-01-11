2a513463963d44b8ae762b846e32eb1dee8fdca3
gefangenimnetz <kontakt@florian-kutschera.de>
2016-01-11 13:26:34 +0100

    Added Android support

---
+++ platforms/android/assets/www/js/date/date.js
@@ -0,0 +1,23 @@
+var date = {
+    node: document.getElementById('widget_datetime'),
+
+    init: function(){
+        moment.locale(CONFIG.date.locale);
+        
+        this.dateNode = document.createElement("span");
+        this.dateNode.classList.add('datetime__date');
+        this.timeNode = document.createElement("span");
+        this.timeNode.classList.add('datetime__time');
+        this.node.appendChild(this.dateNode);
+        this.node.appendChild(this.timeNode);
+        
+        this.interval = setInterval(function() {
+            this.update();
+        }.bind(this), 1000);
+    },
+
+    update: function(){
+        this.dateNode.innerHTML = moment().format("dddd, D. MMMM YYYY");
+        this.timeNode.innerHTML = moment().format("H:mm");
+    }
+};
