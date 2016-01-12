264994144f209bdc706727d20ca42b830bc1039e
gefangenimnetz <kontakt@florian-kutschera.de>
2016-01-12 18:20:30 +0100

    Swapped *ics - based events via device calendar grabbing. Not working in browser obviously.

--- www/js/events/events.js
+++ www/js/events/events.js
@@ -1,22 +1,39 @@
 var events = {
     init: function(){
-        this.fetchICS();
+        //this.fetchICS();
+        this.getLocalDeviceEvents();
     },
 
-    fetchICS: function(){
-        fetch('../js/events/google_birthday.ics').then(function(response) { 
-            return response.text();
-        }).then(function(j) {
-            this.update( j );
-        }.bind(this));
+    onEventsReceivedSuccess: function(data){
+        console.log(data);
+        alert("The next event is: ", data[0].title);
     },
 
-    update: function(data){
-        var root = new ICAL.Component( ICAL.parse(data) );
-        var vevents = root.getAllSubcomponents('vevent');
-        vevents.forEach(function(vevent){
-            var evt = new ICAL.Event(vevent);
-            console.log(evt.startDate.toJSDate(), evt.endDate.toJSDate());
-        });
+    onError: function(msg){
+        alert(msg);
+    },
+
+    getLocalDeviceEvents: function(){
+        var startDate = new Date();
+        var endDate = new Date();
+        endDate.setDate(startDate.getDate() + 14);
+        window.plugins.calendar.findEvent(null, null, null, startDate, endDate, this.onEventsReceivedSuccess, this.onError);
     }
+
+    // fetchICS: function(){
+    //     fetch('../js/events/google_birthday.ics').then(function(response) { 
+    //         return response.text();
+    //     }).then(function(j) {
+    //         this.update( j );
+    //     }.bind(this));
+    // },
+
+    // update: function(data){
+    //     var root = new ICAL.Component( ICAL.parse(data) );
+    //     var vevents = root.getAllSubcomponents('vevent');
+    //     vevents.forEach(function(vevent){
+    //         var evt = new ICAL.Event(vevent);
+    //         console.log(evt.startDate.toJSDate(), evt.endDate.toJSDate());
+    //     });
+    // }
 }
