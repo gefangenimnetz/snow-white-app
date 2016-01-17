b7b0ffe65c01059197dda1f1a8d8e3c58ab38bd6
gefangenimnetz <kontakt@florian-kutschera.de>
2016-01-17 20:59:02 +0100

    Added camera server and updated status bar (solves double defined error)

--- www/js/camera/camera.js
+++ www/js/camera/camera.js
@@ -1,5 +1,21 @@
 var camera = {
     init: function(){
-        
+        if (device.platform === 'browser'){
+            return;
+        }
+
+        cordova.plugins.CameraServer.startServer({
+            'www_root' : '/',
+            'port' : 8080,
+            'localhost_only' : false,
+            'json_info': []
+        }, function( url ){
+            // if server is up, it will return the url of http://<server ip>:port/
+            // the ip is the active network connection
+            // if no wifi or no cell, "127.0.0.1" will be returned.
+            console.log('CameraServer Started @ ' + url); 
+        }, function( error ){
+            console.log('CameraServer Start failed: ' + error);
+        });
     }
 };
