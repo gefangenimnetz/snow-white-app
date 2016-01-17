4d67bfe43abe2996f73a5acab27e123dcf1337e8
gefangenimnetz <kontakt@florian-kutschera.de>
2016-01-17 23:11:33 +0100

    Display camera live feed in app. This will alter be used to make a motion detection. TODO: Use front camera by default.

--- www/js/camera/camera.js
+++ www/js/camera/camera.js
@@ -1,4 +1,7 @@
 var camera = {
+    node: document.getElementById('livecam'),
+
+
     init: function(){
         if (device.platform === 'browser'){
             return;
@@ -10,12 +13,28 @@ var camera = {
             'localhost_only' : false,
             'json_info': []
         }, function( url ){
-            // if server is up, it will return the url of http://<server ip>:port/
-            // the ip is the active network connection
-            // if no wifi or no cell, "127.0.0.1" will be returned.
             console.log('CameraServer Started @ ' + url); 
-        }, function( error ){
+            this.startCameraCapture(url);
+
+        }.bind(this), function( error ){
             console.log('CameraServer Start failed: ' + error);
         });
+    },
+
+    startCameraCapture: function(url){
+        cordova.plugins.CameraServer.startCamera(function(){
+            console.log('Capture Started');
+            this.interval = setInterval(function() {
+                this.updateLiveImage();
+            }.bind(this), 100);
+        }.bind(this),function( error ){
+            console.log('CameraServer StartCapture failed: ' + error);
+        });
+    },
+
+    updateLiveImage: function(){
+        this.node.setAttribute('src', 'http://localhost:8080/live.jpg?rand=' + Math.round(Math.random()*1000000));
     }
+
+
 };
